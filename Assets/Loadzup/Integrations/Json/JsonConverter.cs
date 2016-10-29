73b1dbaf584226a20771437feaccb5137448eb6e
Mathieu Frenette <mfrenette@lvlstudio.com>
2016-10-29 12:55:32 -0400

    Moved ThirdParties -> Plugins. Renamed JsonNet/JsonNetConverter -> Json/JsonConverter.

---
+++ Assets/Loadzup/Integrations/Json/JsonConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Mime;
+using Newtonsoft.Json;
+
+namespace Silphid.Loadzup.JsonNet
+{
+    public class JsonConverter : IConverter
+    {
+        public bool Supports<T>(object data, ContentType contentType) =>
+            data is string && contentType.MediaType == KnownMediaType.ApplicationJson;
+
+        public T Convert<T>(object data, ContentType contentType) =>
+            (T)JsonConvert.DeserializeObject((string)data, typeof(T));
+    }
+}
